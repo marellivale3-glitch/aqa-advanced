41770fe268e2ab26d4ac1ae29adcf20376421277
Valeria Marelli <marellivale3@gmail.com>
2026-07-21 17:42:48 +0300

    Add homework 4.1

---
+++ jsBasics/task1.js
@@ -0,0 +1,14 @@
+const firstName = "Valeria";
+console.log(firstName);
+
+const number = 5;
+console.log(number);
+
+const boolean = true;
+console.log(boolean);
+
+const emptyValue = null;
+console.log(emptyValue);
+
+const notDefined = undefined;
+console.log(notDefined);
---
+++ jsBasics/task2.js
@@ -0,0 +1,9 @@
+const firstName = "Valeria";
+const secondName = "Ivan";
+
+const greeting1 = "Hello " + firstName + " and " + secondName + "!";
+console.log(greeting1);
+
+
+const greeting2 = `Hello ${firstName} and ${secondName}!`;
+console.log(greeting2);
